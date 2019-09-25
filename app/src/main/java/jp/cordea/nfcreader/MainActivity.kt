4c6481bea63407525a9b65db8c48810a453dc494
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-25 21:34:14 +0900

    read nfc tags

--- app/src/main/java/jp/cordea/nfcreader/MainActivity.kt
+++ app/src/main/java/jp/cordea/nfcreader/MainActivity.kt
@@ -1,12 +1,20 @@
 package jp.cordea.nfcreader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = NfcAdapter.getDefaultAdapter(this)
+        adapter.enableReaderMode(
+                this,
+                {
+                },
+                NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_NFC_B,
+                null
+        )
     }
 }
