cbadf2c7bb316744f1ccabdbbf3f6ddb7e1d27ba
Yoshihiro Tanaka <contact@cordea.jp>
2019-09-26 07:26:39 +0900

    read on onResume

--- app/src/main/java/jp/cordea/nfcreader/MainActivity.kt
+++ app/src/main/java/jp/cordea/nfcreader/MainActivity.kt
@@ -1,20 +1,33 @@
 package jp.cordea.nfcreader
 
 import android.nfc.NfcAdapter
+import android.nfc.tech.NfcA
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: NfcAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val adapter = NfcAdapter.getDefaultAdapter(this)
+        adapter = NfcAdapter.getDefaultAdapter(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
         adapter.enableReaderMode(
                 this,
                 {
+                    val nfc = NfcA.get(it)
                 },
                 NfcAdapter.FLAG_READER_NFC_A or NfcAdapter.FLAG_READER_NFC_B,
                 null
         )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        adapter.disableReaderMode(this)
     }
 }
